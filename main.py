c235f16ecd3a598d58542599e31c3ebb2cc7f1d6
chingkingm <chingkingm@gmail.com>
2021-12-10 15:20:12 +0800

    添加天气及敌人json

---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append(r'G:\GenshinTools\HoshinoBot')
+from hoshino import aiorequests
+
+URL_3rdguide = 'https://www.3rdguide.com/web/teamnew/index'
+URL_nga = 'https://bbs.nga.cn/thread.php?fid=549'
+
